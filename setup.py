ca2f27e9c7750716baa951405fcb67e818ccc353
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-11 16:19:49 -0600

    fixing dash syntax error

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-  name='rabbitmq-hpa-exporter',
+  name='rabbitmq_hpa_exporter',
   version='0.1.0',
   description='',
   author='Farmers Edge',
   author_email='lindsay.ablonczy@farmersedge.ca',
-  packages=['rabbitmq-hpa-exporter'],
+  packages=['rabbitmq_hpa_exporter'],
   install_requires=[
     'celery',
     'prometheus_client',
