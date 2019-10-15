2a9c8c41c1761241cdde92a7c2741b888d936c5d
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-15 14:24:45 -0600

    trying to fix syntax error

--- rabbitmq_hpa_exporter/server.py
+++ rabbitmq_hpa_exporter/server.py
@@ -1,10 +1,11 @@
-import time, os
+import time, os, json
 from prometheus_client.core import REGISTRY
 from prometheus_client import start_http_server
 from collector import RabbitmqHpaCollector
 
 def start():
   with open(os.environ.get('RABBITMQ_HPA_EXPORTER_CONFIG')) as config:
+  	config = json.loads(config)
     start_http_server(config.port)
     REGISTRY.register(RabbitmqHpaCollector(config))
     while True:
