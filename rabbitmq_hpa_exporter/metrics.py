9c95a3d03678a2158fda985ee17746e647a748d9
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-18 11:55:23 -0600

    trying to fix issue with gauge not updating

--- rabbitmq_hpa_exporter/metrics.py
+++ rabbitmq_hpa_exporter/metrics.py
@@ -2,10 +2,10 @@ from prometheus_client.core import Gauge
 
 scaleFactor = Gauge("rabbitmqHpaScaleFactor", 
                     "Scale factor for rabbitmq celery worker HPA",
-                    labels=['queue'])
+                    ['queue'])
 workerBusyness = Gauge("celeryWorkerBusyness",
                        "Celery worker busyness from 0 to 1",
-                       labels=['queue'])
+                       ['queue'])
 pubAckRatio = Gauge("rabbitMqPublishAcknowledgementRatio",
                     "Ratio of publish rate to acknowledgement rate",
-                    labels=['queue'])
+                    ['queue'])
