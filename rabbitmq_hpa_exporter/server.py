eb8ecd8fc5a1699ee615f6b5569d929a7e56717b
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-21 15:17:05 -0600

    trying to fix issue with gauge not updating

--- rabbitmq_hpa_exporter/server.py
+++ rabbitmq_hpa_exporter/server.py
@@ -6,5 +6,7 @@ from collector import RabbitmqHpaCollector
 def start():
   with open(os.environ.get('RABBITMQ_HPA_EXPORTER_CONFIG')) as config:
     config = json.loads(config.read())
+    start_http_server(config["port"])
     REGISTRY.register(RabbitmqHpaCollector(config))
-    start_http_server(config["port"], registry=REGISTRY)
+    while True:
+    	time.sleep(1)
