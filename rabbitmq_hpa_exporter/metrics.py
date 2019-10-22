71cb3995d5cf1a62cc34284afaa634f5d8a7270c
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-22 17:40:32 -0600

    fixing syntax error

--- rabbitmq_hpa_exporter/metrics.py
+++ rabbitmq_hpa_exporter/metrics.py
@@ -2,12 +2,12 @@ from prometheus_client.core import GaugeMetricFamily
 
 def getMetrics():
   return {
-  	"rabbitmq_hpa_scale_factor": GaugeMetricFamily("rabbitmq_hpa_scale_factor", 
+    "rabbitmq_hpa_scale_factor": GaugeMetricFamily("rabbitmq_hpa_scale_factor", 
                                                    "Scale factor for rabbitmq celery worker HPA",
                                                    labels=['queue']),
     "celery_worker_busyness": GaugeMetricFamily("celery_worker_busyness",
                                                 "Celery worker busyness from 0 to 1",
-                                                labels=['queue'])
+                                                labels=['queue']),
     "rabbitmq_publish_acknowledgement_ratio": GaugeMetricFamily("rabbitmq_publish_acknowledgement_ratio",
                                                                 "Ratio of publish rate to acknowledgement rate",
                                                                 labels=['queue'])
