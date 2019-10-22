b3ddf17f682cf1e06df93d15b5e23706f3a533be
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-22 14:12:10 -0600

    trying to fix issue with gauge not updating

--- rabbitmq_hpa_exporter/collector.py
+++ rabbitmq_hpa_exporter/collector.py
@@ -44,8 +44,8 @@ class RabbitmqHpaCollector(object):
 
   def collect(self):
     for q in self.data:
-      metrics.workerBusyness.labels(queue=q).set(self.data[q]["busyness"])
-      metrics.pubAckRatio.labels(queue=q).set(self.data[q]["ratio"])
+      metrics.workerBusyness.add_metric([q], self.data[q]["busyness"])
+      metrics.pubAckRatio.add_metric([q], self.data[q]["ratio"])
 
     yield metrics.workerBusyness
     yield metrics.pubAckRatio
--- rabbitmq_hpa_exporter/metrics.py
+++ rabbitmq_hpa_exporter/metrics.py
@@ -1,13 +1,13 @@
-from prometheus_client.core import Gauge, CollectorRegistry
+from prometheus_client.core import GaugeMetricFamily, CollectorRegistry
 
 registry = CollectorRegistry()
 
-scaleFactor = Gauge("rabbitmq_hpa_scale_factor", 
-                    "Scale factor for rabbitmq celery worker HPA",
-                    ['queue'], registry=registry)
-workerBusyness = Gauge("celery_worker_busyness",
-                       "Celery worker busyness from 0 to 1",
-                       ['queue'], registry=registry)
-pubAckRatio = Gauge("rabbitmq_publish_acknowledgement_ratio",
-                    "Ratio of publish rate to acknowledgement rate",
-                    ['queue'], registry=registry)
+scaleFactor = GaugeMetricFamily("rabbitmq_hpa_scale_factor", 
+                                "Scale factor for rabbitmq celery worker HPA",
+                                labels=['queue'], registry=registry)
+workerBusyness = GaugeMetricFamily("celery_worker_busyness",
+                                   "Celery worker busyness from 0 to 1",
+                                   labels=['queue'], registry=registry)
+pubAckRatio = GaugeMetricFamily("rabbitmq_publish_acknowledgement_ratio",
+                                "Ratio of publish rate to acknowledgement rate",
+                                labels=['queue'], registry=registry)
