3574d7ad0be289e3b7d8e0a16d66eade1d6ef0b6
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-22 14:14:44 -0600

    trying to fix issue with gauge not updating

--- rabbitmq_hpa_exporter/metrics.py
+++ rabbitmq_hpa_exporter/metrics.py
@@ -1,13 +1,11 @@
-from prometheus_client.core import GaugeMetricFamily, CollectorRegistry
-
-registry = CollectorRegistry()
+from prometheus_client.core import GaugeMetricFamily
 
 scaleFactor = GaugeMetricFamily("rabbitmq_hpa_scale_factor", 
                                 "Scale factor for rabbitmq celery worker HPA",
-                                labels=['queue'], registry=registry)
+                                labels=['queue'])
 workerBusyness = GaugeMetricFamily("celery_worker_busyness",
                                    "Celery worker busyness from 0 to 1",
-                                   labels=['queue'], registry=registry)
+                                   labels=['queue'])
 pubAckRatio = GaugeMetricFamily("rabbitmq_publish_acknowledgement_ratio",
                                 "Ratio of publish rate to acknowledgement rate",
-                                labels=['queue'], registry=registry)
+                                labels=['queue'])
--- rabbitmq_hpa_exporter/server.py
+++ rabbitmq_hpa_exporter/server.py
@@ -1,12 +1,14 @@
 import time, os, json
 from metrics import registry
 from prometheus_client import start_http_server
+from prometheus_client.core import CollectorRegistry
 from collector import RabbitmqHpaCollector
 
 def start():
   with open(os.environ.get('RABBITMQ_HPA_EXPORTER_CONFIG')) as config:
     config = json.loads(config.read())
     collector = RabbitmqHpaCollector(config)
+    registry = CollectorRegistry()
     registry.register(collector)
     start_http_server(config["port"], registry=registry)
     while True:
