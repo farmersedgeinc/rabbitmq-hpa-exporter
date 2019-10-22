d0fe65f641486a224f718e9cdb7130ec1eb88b53
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-22 14:27:52 -0600

    trying to fix issue with gauge not updating

--- rabbitmq_hpa_exporter/server.py
+++ rabbitmq_hpa_exporter/server.py
@@ -1,15 +1,14 @@
 import time, os, json
 from prometheus_client import start_http_server
-from prometheus_client.core import CollectorRegistry
+from prometheus_client.core import REGISTRY
 from collector import RabbitmqHpaCollector
 
 def start():
   with open(os.environ.get('RABBITMQ_HPA_EXPORTER_CONFIG')) as config:
     config = json.loads(config.read())
     collector = RabbitmqHpaCollector(config)
-    registry = CollectorRegistry()
-    registry.register(collector)
-    start_http_server(config["port"], registry=registry)
+    REGISTRY.register(collector)
+    start_http_server(config["port"], registry=REGISTRY)
     while True:
       collector.calculate()
       time.sleep(5)
