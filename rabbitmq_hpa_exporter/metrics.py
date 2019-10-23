c91c75da2c8879cdcbe5e954563a934d4eec900b
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-23 12:35:40 -0600

    debugging

--- rabbitmq_hpa_exporter/metrics.py
+++ rabbitmq_hpa_exporter/metrics.py
@@ -3,13 +3,16 @@ from prometheus_client.core import GaugeMetricFamily
 def getMetrics(logger):
   logger.debug("TEST")
   return {
-    "rabbitmq_hpa_scale_factor": GaugeMetricFamily("rabbitmq_hpa_scale_factor", 
-                                                   "Scale factor for rabbitmq celery worker HPA",
-                                                   labels=['queue']),
-    "celery_worker_busyness": GaugeMetricFamily("celery_worker_busyness",
-                                                "Celery worker busyness from 0 to 1",
-                                                labels=['queue']),
-    "rabbitmq_publish_acknowledgement_ratio": GaugeMetricFamily("rabbitmq_publish_acknowledgement_ratio",
-                                                                "Ratio of publish rate to acknowledgement rate",
-                                                                labels=['queue'])
+    # "rabbitmq_hpa_scale_factor": GaugeMetricFamily("rabbitmq_hpa_scale_factor", 
+    #                                                "Scale factor for rabbitmq celery worker HPA",
+    #                                                labels=['queue']),
+    # "celery_worker_busyness": GaugeMetricFamily("celery_worker_busyness",
+    #                                             "Celery worker busyness from 0 to 1",
+    #                                             labels=['queue']),
+    # "rabbitmq_publish_acknowledgement_ratio": GaugeMetricFamily("rabbitmq_publish_acknowledgement_ratio",
+    #                                                             "Ratio of publish rate to acknowledgement rate",
+    #                                                             labels=['queue'])
+    "rabbitmq_hpa_scale_factor": "test",
+    "celery_worker_busyness": "test2",
+    "rabbitmq_publish_acknowledgement_ratio": "test3"
   }
