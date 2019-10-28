af79e2f10a2dbbc34a6d6e6b1f3df874266b2ef1
lindsay <lindsay.ablonczy@farmersedge.ca>
2019-10-28 16:43:17 -0600

    fixed bug with decimals

--- rabbitmq_hpa_exporter/collector.py
+++ rabbitmq_hpa_exporter/collector.py
@@ -57,26 +57,16 @@ class RabbitmqHpaCollector(object):
       tempData[r["metric"]["queue"]]["avgBusyness"] = Decimal(r["value"][1])
 
     for q in tempData:
-      if q == "weather_current":
-        self.logger.debug("ratio: {}\nbusyness: {}\ndown: {}\nup: {}".format(tempData[q].get("avgRatio", 1), tempData[q].get("avgBusyness", 1), self.config.get("queues", {}).get(q, {}).get("scaleDownThreshold", 0.5), self.config.get("queues", {}).get(q, {}).get("scaleUpThreshold", 2)))
-        self.logger.debug("{} AND {}".format(tempData[q].get("avgRatio", 1) > self.config.get("queues", {}).get(q, {}).get("scaleUpThreshold", 2), tempData[q]["consumers"] != None))
       try:
         tempData[q]["rabbitmq_publish_acknowledgement_ratio"] = tempData[q]["publish"]/tempData[q]["acknowledge"]
       except:
-        tempData[q]["rabbitmq_publish_acknowledgement_ratio"] = 1
+        tempData[q]["rabbitmq_publish_acknowledgement_ratio"] = Decimal(1)
       tempData[q]["celery_worker_busyness"] = (tempData[q]["reserved"]+tempData[q]["active"])/(tempData[q]["prefetch"]+tempData[q]["concurrency"])
-      if (tempData[q].get("avgRatio", 1) > self.config.get("queues", {}).get(q, {}).get("scaleUpThreshold", 2)) and tempData[q]["consumers"] != None:
-        if q == "weather_current":
-          self.logger.debug("UP")
+      if (tempData[q].get("avgRatio", Decimal(1)) > self.config.get("queues", {}).get(q, {}).get("scaleUpThreshold", Decimal(2))) and tempData[q]["consumers"] != None:
         tempData[q]["rabbitmq_hpa_scale_factor"] = (tempData[q]["consumers"]+self.config.get("queues", {}).get(q, {}).get("scaleAmount", 1))/tempData[q]["consumers"]
-      elif (tempData[q].get("avgBusyness", 1) < self.config.get("queues", {}).get(q, {}).get("scaleDownThreshold", 0.5)) and tempData[q]["consumers"] != None:
-        if q == "weather_current":
-          self.logger.debug("DOWN")
-          self.logger.debug("consumers: {}\nscaleAmount:{}\nresult: {}".format(tempData[q]["consumers"], self.config.get("queues", {}).get(q, {}).get("scaleAmount", 1), (tempData[q]["consumers"]-self.config.get("queues", {}).get(q, {}).get("scaleAmount", 1))/tempData[q]["consumers"]))
+      elif (tempData[q].get("avgBusyness", Decimal(1)) < self.config.get("queues", {}).get(q, {}).get("scaleDownThreshold", Decimal(0.5))) and tempData[q]["consumers"] != None:
         tempData[q]["rabbitmq_hpa_scale_factor"] = (tempData[q]["consumers"]-self.config.get("queues", {}).get(q, {}).get("scaleAmount", 1))/tempData[q]["consumers"]
       else:
-        if q == "weather_current":
-          self.logger.debug("DEFAULTED")
         tempData[q]["rabbitmq_hpa_scale_factor"] = 1
 
     self.data = tempData
